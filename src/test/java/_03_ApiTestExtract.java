a101d02c8f842bff3516a202719e5e11fc962803
Aziz Gunes <azizgunes@gail.com>
2024-11-20 16:29:00 +0300

    apiTestExtract tamamlandı

--- src/test/java/_03_ApiTestExtract.java
+++ src/test/java/_03_ApiTestExtract.java
@@ -1,6 +1,10 @@
 
+import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static io.restassured.RestAssured.*;
 
@@ -58,5 +62,64 @@ public class _03_ApiTestExtract {
 
         System.out.println("limit = " + limit);
         Assert.assertTrue(limit == 10);
+    }
+
+    @Test
+    public void extractingJsonPath4() {
+        // Soru : "https://gorest.co.in/public/v1/users"  endpoint in den dönen
+        // data daki bütün id leri nasıl alırız
+
+        ArrayList<Integer> idler =
+                given()
+                        .when()
+                        .get("https://gorest.co.in/public/v1/users")
+
+                        .then()
+                        .extract().path("data.id")  // PATH i country olan değeri EXTRACT yap
+                ;
+
+        System.out.println("idler = " + idler);
+    }
+
+    @Test
+    public void extractingJsonPath5() {
+        // Soru : "https://gorest.co.in/public/v1/users"  endpoint in den dönen
+        // bütün name leri yazdırınız.
+
+        ArrayList<String> nameler =
+                given()
+                        .when()
+                        .get("https://gorest.co.in/public/v1/users")
+
+                        .then()
+                        .extract().path("data.name")
+                ;
+
+        System.out.println("nameler = " + nameler);
+    }
+
+    @Test
+    public void extractingJsonPathResponsAll() {
+
+        Response donenData =
+                given()
+                        .when()
+                        .get("https://gorest.co.in/public/v1/users")
+
+                        .then()
+                        .extract().response();
+        ;
+
+        List<Integer> idler= donenData.path("data.id");
+        List<String> nameler= donenData.path("data.name");
+        int limit= donenData.path("meta.pagination.limit");
+
+        System.out.println("idler = " + idler);
+        System.out.println("nameler = " + nameler);
+        System.out.println("limit = " + limit);
+
+        //Assert.assertTrue(idler.contains(7522201));
+        Assert.assertTrue(nameler.contains("Varalakshmi Mehrotra"));
+        Assert.assertTrue(limit ==  10);
     }
 }
